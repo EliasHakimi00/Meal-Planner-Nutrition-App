d251bbf3cc9d1535f0fcc86aff8288c8ef2b58ea
DecoMacie <119458392+DecoMacie@users.noreply.github.com>
2023-02-14 18:41:12 +0000

    updated HTML, CSS and JS

---
+++ script.js
@@ -0,0 +1,22 @@
+// $(function() {
+//     $('.lazy').lazy({
+//         placeholder: "data:image/gif;base64,R0lGODlhEALAPQAPzl5uLr9Nrl8e7..."
+//     });
+// });
+
+// const myModal = document.getElementById('myModal')
+// const myInput = document.getElementById('myInput')
+
+// // myModal.addEventListener('shown.bs.modal', () => {
+// //   myInput.focus()
+// // })
+
+// $('.carousel').carousel()
+
+// $(document).ready(function () {
+//   $('.carousel').carousel({
+//       interval: 3000
+//   });
+
+//   $('.carousel').carousel('cycle');
+// });
