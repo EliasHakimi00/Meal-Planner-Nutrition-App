0f65efad7689451c99321f418f402c14bc207987
Elias Hakimi <Elias_Hakimi00@outlook.com>
2023-02-16 18:26:21 +0000

    trying to switch branh, ignore

--- mealdb.js
+++ mealdb.js
@@ -85,4 +85,82 @@ function mealRecipeModal(meal){
 
 // Add ins
 
+// Start of API 2 JS
+// const searchInput = document.getElementById("search-input");
+const searchButton = document.getElementById("search-btn");
+var query;
 
+searchButton.addEventListener("click", function() {
+query = searchInput.value;
+$(document).ready(function() {
+    $.ajax({
+      method: "GET",
+      url: "https://api.api-ninjas.com/v1/nutrition?query=" + query,
+      headers: { "X-Api-Key": "SIId2R42qMuDhuW0nZZ5XmE4xjxBBGmi30DYLuQJ" },
+      contentType: "application/json",
+      success: function(result) {
+        var html = "";
+        for (var i = 0; i < result.length; i++) {
+          html +=
+            "<p>Item Name: " +
+            result[i].name +
+            "<br>Serving Size: " +
+            result[i].serving_size_g +
+            "<br>Calories: " +
+            result[i].calories +
+            "<br>Total Fat (g): " +
+            result[i].fat_total_g +
+            "<br>Total  Saturated Fat (g): " +
+            result[i].fat_saturated_g +
+            "<br>Total Protein (g): " +
+            result[i].protein_g +
+            "<br>Sodium (mg): " +
+            result[i].sodium_mg +
+            "<br>Potassium (mg): " +
+            result[i].potassium_mg +
+            "<br>Cholesterol (mg): " +
+            result[i].cholesterol_mg +
+            "<br>Total Carbohydrates (g): " +
+            result[i].carbohydrates_total_g +
+            "<br>Fibre (g): " +
+            result[i].fiber_g +
+            "<br>Sugar (g): " +
+            result[i].sugar_g +
+            "</p>";
+        }
+        $("#result").html(html);
+      },
+      error: function ajaxError(jqXHR) {
+        console.error("Error: ", jqXHR.responseText);
+        $("#result").html("<p>Error retrieving data</p>");
+      }
+    });
+  });
+})
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal
+btn.onclick = function() {
+modal.style.display = "block";
+};
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+modal.style.display = "none";
+};
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+if (event.target == modal) {
+modal.style.display = "none";
+}
+};
+// End of API 2 JS
